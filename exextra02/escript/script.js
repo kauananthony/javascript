79d0895e5e1c6c02164ed85ced2958ae60219ad2
Kauan Machado <99205320+kauananthony@users.noreply.github.com>
2022-02-12 13:10:41 -0400

    Site de Equações do 2 Grau

---
+++ exextra02/escript/script.js
@@ -0,0 +1,6 @@
+function calcular() {
+    var a = window.document.querySelector('input#a')
+    var b = window.document.querySelector('input#b')
+    var c = window.document.querySelector('input#c')
+    
+}
